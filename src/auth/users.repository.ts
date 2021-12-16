42f58453835f3276c03d08b0c670b79b884cc000
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-15 22:52:07 -0300

    Feature: Signing up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
