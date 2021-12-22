3b1d743520cdd97ee9113ecf1406b0630a62145e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-21 21:34:07 -0300

    Password Hashing With Bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,13 +5,21 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
+    // Hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log('salt', salt);
+    console.log('HashedPassword', hashedPassword);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
